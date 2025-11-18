ece46489e63d07ec1559faaf2c401bf79bc2a287
Sanzhar Baiserikov <2006michigun2006@gmail.com>
2025-11-18 07:59:51 +0500

    Refactor Main class to include Adapter pattern

--- Assignment3_Adapter/Main.java
+++ Assignment3_Adapter/Main.java
@@ -1,18 +1,51 @@
-public class Main {
-    public static void main(String[] args) {
-        OrgansFactory liverFactory = new LiverFactory();
-        Liver liver = liverFactory.createLiver("Brown", "Khovansky", true, 228228);
-        liver.votLiver();
-
+public class Adapter {
+    //Product
+    public static class Heart {
+        private int p;
+        private int b;
+        //Getters and talk()
+        public Heart(int p, int b) {
+            this.p = p;
+            this.b = b;
+        }
+        public int getP() {
+            return p;
+        }
+        public int getB() {
+            return b;
+        }
+    }
+    //Abstract Factory
+    public static abstract interface OrgansFactory {
+        Heart createHeart(int p, int b);
+    }
+    //Concrete Factory
+    public static class HeartFactory implements OrgansFactory {
+        @Override
+        public Heart createHeart(int p, int b) {
+            return new Heart(p, b);
+        }
+    }
+    //Abstract Adapter
+    public static interface OrgansAdapter {
+        void operate();
+    }
+    //Concrete Adapter
+    public static class HeartToEngine implements OrgansAdapter {
+        Heart heart;
+        public HeartToEngine(Heart heart) {
+            this.heart = heart;
+        }
+        @Override
+        public void operate() {
+            System.out.println("It goes in " + heart.getP() * 100 + " in " + heart.getB() / 10 + " seconds.");
+        }
+    }
+    //Client
+    public static void main(String[] poop) {
         OrgansFactory heartFactory = new HeartFactory();
-        Heart heart = heartFactory.createHeart("Red", "Dimash", 5, 1337228);
-        heart.votHeart();
-
-        Engine engine = new Engine("BMW V8", "4-tonks", 30, 1337228);
-        OrgansFactory engineAdapter = new EngineAdapter(engine);
-
-        Heart engineHeart = engineAdapter.createHeart("BMW", "2-tanks", 20, 1337228);
-        engineHeart.votHeart();
-        ((EngineAdapter)engineAdapter).delaemEngine().votEngine();
+        Heart heart = heartFactory.createHeart(10000, 50);
+        OrgansAdapter engine = new HeartToEngine(heart);
+        engine.operate();
     }
 }
